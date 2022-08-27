444aaa4444b53b83d18bc230c63e34160a5ea4f8
Bartosz Koscielniak <bartosz.ko@modulesgarden.com>
2022-08-27 15:59:47 +0200

    ADD: user controller

---
+++ src/main/java/com/example/cryptoexchange/user/UserController.java
@@ -0,0 +1,44 @@
+package com.example.cryptoexchange.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers(){
+        return userService.getUsers();
+    }
+
+    @PostMapping
+    public void registerUser(@RequestBody User user){
+        userService.registerUser(user);
+    }
+
+    @DeleteMapping(path = "{userId}")
+    public void deleteUser(@PathVariable("userId") Long id){
+        userService.deleteUser(id);
+    }
+
+    @PutMapping(path = "{userId}")
+    public void updateUser(
+            @PathVariable("userId") Long id,
+            @RequestParam(required = false) String firstName,
+            @RequestParam(required = false) String lastName,
+            @RequestParam(required = false) String email,
+            @RequestParam(required = false) String contactNumber){
+
+        userService.updateUser(id, firstName, lastName, email, contactNumber);
+    }
+}
