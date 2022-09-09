04d531280f78711a040107a88872acc5cab96e07
Bartosz Koscielniak <bartosz.ko@modulesgarden.com>
2022-09-10 00:38:09 +0200

    ADD: Wallet controller

---
+++ src/main/java/com/example/cryptoexchange/wallet/WalletController.java
@@ -0,0 +1,17 @@
+package com.example.cryptoexchange.wallet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/wallet")
+public class WalletController {
+
+    private final WalletService walletService;
+
+    @Autowired
+    public WalletController(WalletService walletService) {
+        this.walletService = walletService;
+    }
+}
