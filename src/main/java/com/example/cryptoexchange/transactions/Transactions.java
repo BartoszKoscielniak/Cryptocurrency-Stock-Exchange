651eb223e32e4cc0eec2273fdf798cf94c147ae1
Bartosz Koscielniak <bartosz.ko@modulesgarden.com>
2022-09-10 00:39:58 +0200

    ADD: Transactions model

---
+++ src/main/java/com/example/cryptoexchange/transactions/Transactions.java
@@ -0,0 +1,37 @@
+package com.example.cryptoexchange.transactions;
+
+import com.example.cryptoexchange.cryptocurrency.Cryptocurrency;
+import com.example.cryptoexchange.wallet.Wallet;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transactions {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    private LocalDateTime transactionDate;
+    @Enumerated(EnumType.STRING)
+    private TransactionsStatus transactionsStatus;
+    private Double quantity;
+    private Double exchangeRate;
+
+    @ManyToOne
+    @JoinColumn(name = "cryptocurrency_id")
+    private Cryptocurrency cryptocurrency;
+
+    @ManyToOne
+    @JoinColumn(name = "wallet_id")
+    private Wallet wallet;
+
+
+
+}
