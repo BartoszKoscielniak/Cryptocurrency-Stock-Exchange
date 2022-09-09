0f70040955a94e1dc011a3cbe021eb2e3270af5a
Bartosz Koscielniak <bartosz.ko@modulesgarden.com>
2022-09-10 00:39:23 +0200

    ADD: Cryptocurrency controller

---
+++ src/main/java/com/example/cryptoexchange/cryptocurrency/CryptocurrencyController.java
@@ -0,0 +1,26 @@
+package com.example.cryptoexchange.cryptocurrency;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/cryptocurrency")
+public class CryptocurrencyController {
+
+    private final CryptocurrencyService cryptocurrencyService;
+
+    @Autowired
+    public CryptocurrencyController(CryptocurrencyService cryptocurrencyService) {
+        this.cryptocurrencyService = cryptocurrencyService;
+    }
+
+    @GetMapping
+    public List<Cryptocurrency> getCryptocurrencies() {
+        return cryptocurrencyService.getCryptocurrencies();
+    }
+
+}
