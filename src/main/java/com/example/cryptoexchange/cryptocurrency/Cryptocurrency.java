ef2dc410802c13a1032605d784989e65d7384683
Bartosz Koscielniak <bartosz.ko@modulesgarden.com>
2022-09-10 00:39:16 +0200

    ADD: Cryptocurrency model

---
+++ src/main/java/com/example/cryptoexchange/cryptocurrency/Cryptocurrency.java
@@ -0,0 +1,68 @@
+package com.example.cryptoexchange.cryptocurrency;
+
+import com.example.cryptoexchange.transactions.Transactions;
+import com.example.cryptoexchange.walletdetails.WalletDetails;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cryptocurrency {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    Long id;
+    String name;
+    String symbol;
+
+    @OneToMany(mappedBy = "cryptocurrency")
+    Set<WalletDetails> walletDetails;
+
+    @OneToMany(mappedBy = "cryptocurrency")
+    Set<Transactions> transactions;
+
+    public Cryptocurrency(String name, String symbol) {
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    public Long getId( ) {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName( ) {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSymbol( ) {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public String toString( ) {
+        return "Cryptocurrency{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", symbol='" + symbol + '\'' +
+                '}';
+    }
+}
