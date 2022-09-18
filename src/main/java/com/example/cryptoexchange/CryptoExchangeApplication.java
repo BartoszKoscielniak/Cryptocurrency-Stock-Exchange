fc4a38028452ef9b15c3b02b2606d2b5ab2c36a4
Bartosz Koscielniak <bartosz.ko@modulesgarden.com>
2022-09-18 20:17:14 +0200

    ADD: swaggerui URL

--- src/main/java/com/example/cryptoexchange/CryptoExchangeApplication.java
+++ src/main/java/com/example/cryptoexchange/CryptoExchangeApplication.java
@@ -2,10 +2,13 @@ package com.example.cryptoexchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class CryptoExchangeApplication {
-
+    //http://localhost:8080/swagger-ui/index.html#/
     public static void main(String[] args) {
         SpringApplication.run(CryptoExchangeApplication.class, args);
     }
